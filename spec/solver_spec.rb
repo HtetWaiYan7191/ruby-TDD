ecd35346ea15dd12a674176e669d0e72da66883c
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-09-05 15:04:36 +0630

    Add test for number greater than 1

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -8,12 +8,16 @@ describe Solver do
         end
 
         it " the factorial method will return 1 if the number is equal to 0" do
+            expect(Solver.new.factorial(0)).to eq 1
+        end
+
+        it " the factorial method will return 1 if the number is equal to 1" do
             expect(Solver.new.factorial(1)).to eq 1
         end
 
-        # it "The factorial method will return the factorial value of a number greater than 0" do
-        #     expect(Solver.new.factorial(4)).to eq 24
-        # end
+        it "The factorial method will return the factorial value of a number greater than 0" do
+            expect(Solver.new.factorial(4)).to eq 24
+        end
 
 
     end
