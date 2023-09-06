92b7513ed06f2f5db6ebcd41e9e94ae65413dc98
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-09-06 12:01:25 +0630

    Add test for second condition

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -30,5 +30,13 @@ describe Solver do
         it "should return 'fizzbuzz' when input is divisible by 3 and 5" do
             expect(Solver.new.fizzbuzz(15)).to eq "fizzbuzz"
         end
+
+        it "should return 'fizz' when input is divisible only by 3" do
+            expect(Solver.new.fizzbuzz(3)). to eq "fizz"
+        end
+
+        it "should return 'buzz' when input is divisible only by 5" do
+            expect(Solver.new.fizzbuzz(5)).to eq "buzz"
+        end
     end
 end
