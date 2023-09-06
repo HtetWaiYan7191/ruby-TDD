d707ba6572c660d88e9d17fc882b45d9bf439888
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-09-06 12:07:24 +0630

    Fix rubocop errors

--- solver.rb
+++ solver.rb
@@ -1,28 +1,27 @@
 class Solver
-    def factorial(n)
-        if n.negative?
-             "Please enter a number greater than or equal to 0"
-        elsif [0, 1].include?(n)
-            1
-        else
-            n * factorial(n-1)
-        end
+  def factorial(num)
+    if num.negative?
+      'Please enter a number greater than or equal to 0'
+    elsif [0, 1].include?(num)
+      1
+    else
+      num * factorial(num - 1)
     end
+  end
 
-    def reverse(word)
-        word.reverse
+  def reverse(word)
+    word.reverse
+  end
+
+  def fizzbuzz(num)
+    if (num % 3).zero? && (num % 5).zero?
+      'fizzbuzz'
+    elsif (num % 3).zero?
+      'fizz'
+    elsif (num % 5).zero?
+      'buzz'
+    else
+      num.to_s
     end
-
-    def fizzbuzz(n)
-        if n % 3 == 0 && n % 5 == 0
-            'fizzbuzz'
-        elsif n % 3 == 0
-            'fizz'
-        elsif n % 5 == 0
-            'buzz'
-        else
-            n.to_s  
-        end
-
-    end
+  end
 end
