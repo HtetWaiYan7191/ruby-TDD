3a654d19d92a9b655852bc3b2f9d2ed5cd6fc419
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-09-07 15:10:40 +0630

    Update the tests

--- solver.rb
+++ solver.rb
@@ -9,19 +9,5 @@ class Solver
     end
   end
 
-  def reverse(word)
-    word.reverse
-  end
-
-  def fizzbuzz(num)
-    if (num % 3).zero? && (num % 5).zero?
-      'fizzbuzz'
-    elsif (num % 3).zero?
-      'fizz'
-    elsif (num % 5).zero?
-      'buzz'
-    else
-      num.to_s
-    end
-  end
+ 
 end
